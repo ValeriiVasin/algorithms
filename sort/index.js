7fb7c806ee077b5c2fc035997f2f7b2ebfd8125c
Valerii Vasin <valerii.vasin@gmail.com>
2014-08-20 19:55:43 +0400

    Initial commit. Insertion sort.

---
+++ sort/index.js
@@ -0,0 +1,5 @@
+var arr = require('./data').arr;
+var insertionSort = require('./insertion');
+
+console.log('before:', arr);
+console.log('after: ', insertionSort(arr));
---
+++ sort/insertion.js
@@ -0,0 +1,23 @@
+/**
+ * http://en.wikipedia.org/wiki/Insertion_sort
+ */
+
+function sort(arr) {
+  var length = arr.length;
+  var i, j, value;
+
+  for (i = 1; i < length; i += 1) {
+    j = i;
+    value = arr[i];
+    while ( j >= 0 && value < arr[j - 1] ) {
+      arr[j] = arr[j - 1];
+      j -= 1;
+    }
+
+    arr[j] = value;
+  }
+
+  return arr;
+}
+
+module.exports = sort;
