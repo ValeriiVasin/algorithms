fc6a4028c84091ce64032ab5449a4481f30ce515
Valerii Vasin <valerii.vasin@gmail.com>
2014-08-21 00:29:16 +0400

    Optimize bubble algorithm for last swap.

--- sort/bubble.js
+++ sort/bubble.js
@@ -1,18 +1,20 @@
 function sort(arr) {
   var length = arr.length;
-  var swapped, temp, i;
+  var lastSwap, temp, i;
 
   do {
-    swapped = false;
+    lastSwap = 0;
     for (i = 0; i < length - 1; i += 1 ) {
       if ( arr[i] > arr[i + 1] ) {
         temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-        swapped = true;
+        lastSwap = i;
       }
     }
-  } while (swapped);
+
+    length = lastSwap;
+  } while (lastSwap !== 0);
 
   return arr;
 }
