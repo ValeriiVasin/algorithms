f63e39d5e4afb43cb8a1edadfa28eed6f0588716
Valerii Vasin <valerii.vasin@gmail.com>
2014-10-03 01:55:08 +0400

    Remove duplicate points and refactor.

--- algs4/week3/assignment/Fast.java
+++ algs4/week3/assignment/Fast.java
@@ -5,19 +5,44 @@ public class Fast {
     private static int n;
     private static Point[] points;
 
+    // read points, sort, remove duplicates
     private static void read(String filename) {
       In file = new In(filename);
 
       n = file.readInt();
       points = new Point[n];
 
-      int i = 0;
+      int j = 0;
       while (!file.isEmpty()) {
         int x = file.readInt();
         int y = file.readInt();
 
-        points[i++] = new Point(x, y);
+        points[j++] = new Point(x, y);
       }
+
+      // sort
+      Arrays.sort(points);
+      StdOut.println(points.length);
+
+      // remove duplicates
+      Point[] temp = new Point[n];
+      int index = 0;
+      temp[0] = points[0];
+      for (int i = 1; i < n; i++) {
+
+        // add if not equal to previously saved
+        if (temp[index].compareTo(points[i]) != 0) {
+          index++;
+          temp[index] = points[i];
+        }
+      }
+
+      points = new Point[index + 1];
+      for (int i = 0; i <= index; i++) {
+        points[i] = temp[i];
+      }
+
+      StdOut.println(points.length);
     }
 
     private static void output(Point p, Point[] points, int lastIndex, int count) {
@@ -30,7 +55,7 @@ public class Fast {
         arr[index++] = points[i];
       }
 
-      Insertion.sort(arr);
+      Merge.sort(arr);
 
       Point pFirst = arr[0];
       Point pLast = arr[arr.length - 1];
@@ -72,36 +97,41 @@ public class Fast {
       StdDraw.setYscale(0, 32768);
 
       // draw all points
-      for (int i = 0; i < n; i++) {
+      for (int i = 0; i < points.length; i++) {
         points[i].draw();
       }
 
-      for (int i = 0; i < n - 3; i++) {
+      for (int i = 0; i < points.length; i++) {
         Point p = points[i];
-
-        Point[] sorted = new Point[n - 1];
+        Point[] sortedBySlope = new Point[points.length - 1];
         int index = 0;
-        for (int j = 0; j < n; j++) {
+
+        // copy other points to sortedBySlope array
+        for (int j = 0; j < points.length; j++) {
 
           if (j != i) {
-            sorted[index++] = points[j];
+            sortedBySlope[index++] = points[j];
           }
         }
 
-        Arrays.sort(sorted, p.SLOPE_ORDER);
+        // sort by p-slope
+        Arrays.sort(sortedBySlope, p.SLOPE_ORDER);
 
+        // find segments with more than 3 points in a row with same slope
         int count = 1;
-        double previousSlopeTo = p.slopeTo(sorted[0]);
+        double previousSlopeTo = p.slopeTo(sortedBySlope[0]);
         double currentSlopeTo;
-        for (int j = 1; j < sorted.length; j++) {
-          currentSlopeTo = p.slopeTo(sorted[j]);
+        for (int j = 1; j < sortedBySlope.length; j++) {
+          currentSlopeTo = p.slopeTo(sortedBySlope[j]);
 
           if (currentSlopeTo == previousSlopeTo) {
             count++;
           } else {
             if (count >= 3) {
-              output(p, sorted, j - 1, count);
+              output(p, sortedBySlope, j - 1, count);
             }
+
+            // reset count
             count = 1;
           }
 
