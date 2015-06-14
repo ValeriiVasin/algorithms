97a0c4de05f632c9df9257cd7cc12de134215141
Valerii Vasin <valerii.vasin@gmail.com>
2015-06-14 12:28:47 +0200

    Solve euler#4

---
+++ euler/4.js
@@ -0,0 +1,61 @@
+var number = 999 * 999;
+
+function isPalindrome(number) {
+  var stringified = String(number);
+  var reversed = stringified.split('').reverse().join('');
+
+  return stringified === reversed;
+}
+
+function getLargestPalindrome() {
+  var value;
+  var palindrome;
+
+  console.time('optimal');
+  for (var i = 999; i >= 100; i--) {
+    if (palindrome && palindrome > i * i) {
+      break;
+    }
+
+    for (var j = i; j >= 100; j--) {
+      value = i * j;
+
+      if (palindrome && value < palindrome) {
+        break;
+      }
+
+      if (isPalindrome(value)) {
+        palindrome = value;
+
+        // other values in this loop is lower
+        break;
+      }
+    }
+  }
+  console.timeEnd('optimal');
+
+  return palindrome;
+}
+
+function getLargestPalindromeNonOptimal() {
+  var palindrome = 0;
+
+  console.time('non optimal');
+  for (var i = 999; i >= 100; i--) {
+    for (var j = 999; j >= 100; j--) {
+      var value = i * j;
+
+      if (isPalindrome(value) && value > palindrome) {
+        palindrome = value;
+      }
+    }
+  }
+  console.timeEnd('non optimal');
+
+  return palindrome;
+}
+
+console.log(
+  getLargestPalindrome(9801),
+  getLargestPalindromeNonOptimal()
+)
