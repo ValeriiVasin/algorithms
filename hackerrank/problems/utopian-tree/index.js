f435d7dda0bbe47e428c2a8706dc4a1dd793c66f
Valerii Vasin <valerii.vasin@gmail.com>
2015-11-17 23:37:02 +0100

    [hackerrank] Solve utopian-tree.

---
+++ hackerrank/problems/utopian-tree/index.js
@@ -0,0 +1,22 @@
+import { read } from '../../lib/read';
+
+const solve = (reader) => {
+  let n = reader.readNumber();
+
+  for (let i = 0; i < n; i += 1) {
+    let cycles = reader.readNumber();
+    console.log(utopianTreeHeight(cycles));
+  }
+};
+
+const utopianTreeHeight = (n) => {
+  let height = 1;
+
+  for (let i = 0; i < n; i += 1) {
+    height = i % 2 === 0 ? height * 2 : height + 1;
+  }
+
+  return height;
+};
+
+read().then(solve);
