16d13680544dd59fae706ba326ac7f6208e2c45a
Valerii Vasin <valerii.vasin@gmail.com>
2015-11-21 00:54:49 +0100

    [hackerrank] Solve 2s-complement properly.

--- hackerrank/problems/2s-complement/index.js
+++ hackerrank/problems/2s-complement/index.js
@@ -10,9 +10,12 @@ const solve = (reader) => {
   let ranges = [];
   for (let i = 0; i < T; i++) {
     let [start, end] = reader.readNumbers(2);
-    ranges.push({ start, end });
+    // ranges.push({ start, end });
+    console.log(diff(start, end, 32));
   }
+}
 
+const _solveWithRanges = (ranges) => {
   let realRangesMap = getRealRangesMap(ranges);
 
   for (let range of ranges) {
@@ -38,8 +41,13 @@ const solve = (reader) => {
     }
     console.log(result);
   }
-}
+};
 
+/**
+ * Get ranges uniq ranges that is supposed to be processed
+ * [-3, 3], [-1, 1] => [-3, -1], [-1, 1], [1, 3]
+ *
+ */
 export const getRealRanges = (ranges) => {
   let dots = new Set();
 
@@ -124,7 +132,7 @@ const negate = (str) => {
 }
 
 let rangeCache = new Map();
-const amountOfOnes = (start, end) => {
+export const amountOfOnes = (start, end) => {
   let key = `${start}:${end}`;
 
   if (rangeCache.has(key)) {
@@ -149,6 +157,7 @@ const amountOfOnesInNumber = (number) => {
   }
 
   let binary = toComplement(number, 32);
+  console.log(binary);
   let result = 0;
 
   for (let letter of binary) {
@@ -161,5 +170,95 @@ const amountOfOnesInNumber = (number) => {
 
   return result;
 };
+
+//
+// NEW STUFF!
+//
+const _bitsNeeded = (n) => {
+  let bits = 0;
+
+  while (n) {
+    bits++;
+    n >>= 1;
+  }
+
+  return bits;
+}
+
+// amount of ones that are used in a range [0..n]
+export const onesFromZero = (n) => {
+  let bits = _bitsNeeded(n);
+
+  // for zero-variang
+  n += 1;
+
+  // whole cycle length (after how many cycles it will repeat)
+  // for first bit its = 2, second - 4 etc
+  // its power of 2 for bit shifts
+  let cycle = 2;
+
+  // sum
+  let sum = 0;
+  while (bits) {
+    let frequency = cycle / 2;
+
+    // whole cycle repeated times
+    let full = Math.floor(n / cycle);
+
+    // reminder from cycle repeats
+    let reminder = n % cycle;
+
+
+    let bitChanges = (full * frequency) +
+                     (reminder > frequency ? reminder - frequency : 0);
+
+    sum += bitChanges;
+    cycle *= 2;
+    bits -= 1;
+  }
+
+  return sum;
+}
+
+/**
+ * Get amount of "1" needed for positive range: 0 <= start <= end
+ */
+export const diffFromZero = (start, end) => {
+  return onesFromZero(end) - onesFromZero(start === 0 ? 0 : start - 1);
+};
+
+/**
+ * Get amount of "1" needed for negative range: -MAX <= start <= end <= -1
+ */
+export const onesFromMinimum = (n, bits) => {
+  let min = -Math.pow(2, bits - 1);
+
+  if (n <= -1) {
+    // get positive diff
+    let diff = n - min;
+
+    // every negative number starts from "1" - minus sign
+    let onesFromMinuses = diff + 1;
+
+    return onesFromMinuses + onesFromZero(diff);
+  }
+
+  return onesFromMinimum(-1, bits) + onesFromZero(n);
+}
+
+// amount of ones needed for the range
+export const diff = (start, end, bits) => {
+  if (start >= 0) {
+    return diffFromZero(start, end);
+  }
+
+  let min = -Math.pow(2, bits - 1);
+
+  if (start === min) {
+    return onesFromMinimum(end, bits);
+  }
+
+  return onesFromMinimum(end, bits) - onesFromMinimum(start - 1, bits);
+}
 
 read().then(solve);
--- hackerrank/spec/2s-complement.spec.js
+++ hackerrank/spec/2s-complement.spec.js
@@ -22,6 +22,12 @@ const dictionary = new Map([
 const toComplement = require('../build/2s-complement').toComplement;
 const getRealRanges = require('../build/2s-complement').getRealRanges;
 const getRealRangesMap = require('../build/2s-complement').getRealRangesMap;
+const amountOfOnes = require('../build/2s-complement').amountOfOnes;
+
+const onesFromZero = require('../build/2s-complement').onesFromZero;
+const diffFromZero = require('../build/2s-complement').diffFromZero;
+const onesFromMinimum = require('../build/2s-complement').onesFromMinimum;
+const diff = require('../build/2s-complement').diff;
 
 describe('2s-complement', () => {
   describe('toComplement()', () => {
@@ -59,5 +65,66 @@ describe('2s-complement', () => {
         [ 3,  5 ]
       ]);
     });
+  });
+
+  //0000 - 0 - 0
+  //0001 - 1 - 1
+  //0010 - 2 - 2
+  //0011 - 3 - 4
+  //0100 - 4 - 5
+  //0101 - 5 - 7
+  //0110 - 6 - 9
+  //0111 - 7 - 12
+  //1000 - 8 - 13
+  it('onesFromZero()', () => {
+    expect(onesFromZero(1)).toBe(1);
+    expect(onesFromZero(2)).toBe(2);
+    expect(onesFromZero(3)).toBe(4);
+    expect(onesFromZero(4)).toBe(5);
+    expect(onesFromZero(5)).toBe(7);
+    expect(onesFromZero(6)).toBe(9);
+    expect(onesFromZero(7)).toBe(12);
+    expect(onesFromZero(8)).toBe(13);
+  });
+
+  it('diffFromZero()', () => {
+    expect(diffFromZero(0, 0)).toBe(0);
+    expect(diffFromZero(1, 1)).toBe(1);
+    expect(diffFromZero(0, 1)).toBe(1);
+    expect(diffFromZero(2, 7)).toBe(11);
+
+    expect(diffFromZero(Math.pow(2, 4) - 1, Math.pow(2, 4) - 1)).toBe(4);
+    expect(diffFromZero(Math.pow(2, 31) - 1, Math.pow(2, 31) - 1)).toBe(31);
+  });
+
+  it('onesFromMinimum()', () => {
+    expect(onesFromMinimum(-8, 4)).toBe(1);
+    expect(onesFromMinimum(-7, 4)).toBe(3);
+    expect(onesFromMinimum(-6, 4)).toBe(5);
+    expect(onesFromMinimum(-5, 4)).toBe(8);
+    expect(onesFromMinimum(-1, 4)).toBe(20);
+
+    expect(onesFromMinimum(0, 4)).toBe(20);
+    expect(onesFromMinimum(1, 4)).toBe(21);
+
+    expect(onesFromMinimum(-Math.pow(2, 31), 32)).toBe(1);
+  });
+
+  it('diff()', () => {
+    expect(diff(0, 0)).toBe(0);
+    expect(diff(1, 1)).toBe(1);
+    expect(diff(0, 1)).toBe(1);
+    expect(diff(2, 7)).toBe(11);
+
+    expect(diff(-8, -8, 4)).toBe(1);
+    expect(diff(-8, -7, 4)).toBe(3);
+
+    expect(diff(-2, 0, 32)).toBe(63);
+    expect(diff(-3, 4, 32)).toBe(99);
+    expect(diff(-1, 4, 32)).toBe(37);
+
+    expect(diff(-Math.pow(2, 31), -Math.pow(2, 31), 32)).toBe(1);
+    expect(diff(-Math.pow(2, 31), -Math.pow(2, 31) + 1, 32)).toBe(3);
+    expect(diff(-1, 0, 32)).toBe(32);
   });
 });
