97c06973e945a734685899550b05a0be2fc81552
Valerii Vasin <valerii.vasin@gmail.com>
2017-06-20 13:50:51 +0200

    Improve solution

--- leetcode/two-sum.js
+++ leetcode/two-sum.js
@@ -1,12 +1,15 @@
 // https://leetcode.com/problems/two-sum/#/description
+
 export const twoSum = (nums, target) => {
   const length = nums.length;
 
-  for (let i = 0; i < length - 1; i += 1) {
-    for (let j = i + 1; j < length; j += 1) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+  for (let i = 0; i < length; i++) {
+    const element = nums[i];
+    const needle = target - element;
+    const needleIndex = nums.indexOf(needle, i + 1);
+
+    if (needleIndex !== -1) {
+      return [i, needleIndex];
     }
   }
 
--- leetcode/two-sum.spec.js
+++ leetcode/two-sum.spec.js
@@ -1,5 +1,9 @@
 const { twoSum } = require('./two-sum');
 
-test('hello there', () => {
+test('one', () => {
   expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test('two', () => {
+  expect(twoSum([3,2,4], 6)).toEqual([1, 2]);
 });
