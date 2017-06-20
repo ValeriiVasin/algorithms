828a47b901b0b2fc437d857587f04368cd7bd82a
Valerii Vasin <valerii.vasin@gmail.com>
2017-06-20 14:46:06 +0200

    Improve result

--- leetcode/two-sum.js
+++ leetcode/two-sum.js
@@ -1,15 +1,39 @@
 // https://leetcode.com/problems/two-sum/#/description
 
+const toHash = nums => nums.reduce((hash, num, index) => {
+  // save the index of the first element
+  if (!hash.hasOwnProperty(num)) {
+    hash[num] = index;
+  }
+
+  return hash;
+}, {});
+
 export const twoSum = (nums, target) => {
+  const hash = toHash(nums);
   const length = nums.length;
+
+  // check the edge case - needle is the half of target and there is few of them
+  if (target % 2 === 0 && hash.hasOwnProperty(target / 2)) {
+    const needle = target / 2;
+    const needleIndex = nums.indexOf(needle, hash[needle] + 1);
+
+    if (needleIndex !== -1) {
+      return [hash[needle], needleIndex];
+    }
+  }
 
   for (let i = 0; i < length; i++) {
     const element = nums[i];
     const needle = target - element;
-    const needleIndex = nums.indexOf(needle, i + 1);
 
-    if (needleIndex !== -1) {
-      return [i, needleIndex];
+    // we are guaranteed that elements are uniq
+    if (needle === element) {
+      continue;
+    }
+
+    if (hash.hasOwnProperty(needle)) {
+      return [i, hash[needle]];
     }
   }
 
--- leetcode/two-sum.spec.js
+++ leetcode/two-sum.spec.js
@@ -1,9 +1,13 @@
 const { twoSum } = require('./two-sum');
 
-test('one', () => {
+test('simple case', () => {
   expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
 });
 
-test('two', () => {
+test('do not use same element twice', () => {
   expect(twoSum([3,2,4], 6)).toEqual([1, 2]);
+});
+
+test('there could be few elements with same value', () => {
+  expect(twoSum([3, 3], 6)).toEqual([0, 1]);
 });
